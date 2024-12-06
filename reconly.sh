a5063f29b02d8af21554ea081139c91980983601
Ln0rag <72595035+Ln0rag@users.noreply.github.com>
2024-12-06 11:32:30 +0200

    reconly.sh

--- reconly.sh
+++ reconly.sh
@@ -1,6 +1,6 @@
-#!/bin/bash                               
+#!/bin/bash
 
-# Define different color codes
+# color codes
 color_reset="\e[0m"
 color_bright_cyan="\e[36;1m"
 color_bright_red="\e[31;1m"
@@ -9,16 +9,17 @@ color_bright_green="\e[32;1m"
 color_bright_magenta="\e[35;1m"
 
 # Variables
-output_dir="$HOME/Desktop"
+output_dir="$HOME/reconly"
 domain=""
 shodanAPI=""
 wordlist=""
-gowitness_output_dir="$output_dir/7_gowitness"
+gowitness_output_dir="$output_dir/gowitness"
 use_amass=""
+use_shodan=""
 
-# Function to display help
+# display help
 display_help() {
-    echo "Usage: $0 -d <domain> -k <shodan_api_key> -w <path_to_wordlist>"
+    echo "Usage: $0 -d <domain> -k <shodan_api_key> -w <path_to_wordlist.txt>"
     echo "Options:"
     echo "  -d    Specify the domain (required)"
     echo "  -k    Provide your Shodan API key (optional, will prompt if not provided)"
@@ -27,23 +28,22 @@ display_help() {
 }
 
 # Greeting banner
-echo -e "$color_bright_cyan
-      ██████╗  ███████╗  ██████╗  ██████╗  ███╗   ██╗ ██╗   ██╗   ██╗
-      ██╔══██╗ ██╔════╝ ██╔════╝ ██╔═══██╗ ████╗  ██║ ██║   ╚██╗ ██╔╝
-      ██████╔╝ █████╗   ██║      ██║   ██║ ██╔██╗ ██║ ██║    ╚████╔╝
-      ██╔══██╗ ██╔══╝   ██║      ██║   ██║ ██║╚██╗██║ ██║     ╚██╔╝
-      ██║  ██║ ███████╗ ╚██████╗ ╚██████╔╝ ██║ ╚████║ ███████╗ ██║
-      ╚═╝  ╚═╝ ╚══════╝  ╚═════╝  ╚═════╝  ╚═╝  ╚═══╝ ╚══════╝ ╚═╝
+echo -e "$color_bright_red
+  ______ _______ _______  _____  __   _        __   __
+ |_____/ |______ |       |     | | \  | |        \_/  
+ |    \_ |______ |_____  |_____| |  \_| |_____    |   
 $color_reset"
 
-echo -e "$color_white                                   github.com/Ln0rag$color_reset"
+echo -e "$color_white                 github.com/Ln0rag$color_reset"
 echo -e ""
 
-# Add to the beginning of your script to set the output directory from an env variable or default
-output_dir="${OUTPUT_DIR:-$HOME/Desktop}"
+# Creating output files directory if not exists
+if [ ! -d "$output_dir" ]; then
+    mkdir -p "$output_dir"
+fi
 
-# Add logging functionality
-log_file="$output_dir/0script.log"
+# logging functionality
+log_file="$output_dir/reconly.log"
 exec &> >(tee -a "$log_file")
 
 # Parse command line options
@@ -68,9 +68,9 @@ while getopts "d:k:w:h" opt; do
     esac
 done
 
-# Validate domain format
+# domain format validation
 if [[ ! "$domain" =~ ^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
-    echo "Error: Invalid domain format. Example: domain.com"
+    echo "Invalid domain format, Example: domain.com"
     exit 1
 fi
 
@@ -80,51 +80,41 @@ if [ -z "$domain" ]; then
     exit 1
 fi
 
-# Check if API key is provided
-if [ -z "$shodanAPI" ]; then
+# Ask if the user wants to use Shodan
+read -p "Do you want to search in Shodan? (y/n): " use_shodan
+if [[ "$use_shodan" =~ ^[Yy]$ ]]; then
     read -p "Enter your Shodan API Key: " shodanAPI
 fi
 
-# Check if wordlist is provided
-if [ -z "$wordlist" ]; then
+# Ask if the user wants to use subdomain enumeration
+read -p "Do you want to use subdomain enumeration? (y/n): " use_amass
+if [[ "$use_amass" =~ ^[Yy]$ ]]; then
     read -p "Enter the path to your wordlist file for subdomain brute-forcing: " wordlist
     wordlist="${wordlist/#\~/$HOME}"  # Expand ~ to $HOME
+    
+    # Validate wordlist file
+    if [ ! -f "$wordlist" ]; then
+        echo "Wordlist file not found at '$wordlist'."
+        exit 1
+    fi
 fi
 
-# Validate wordlist file
-if [ ! -f "$wordlist" ]; then
-    echo "Error: Wordlist file not found at '$wordlist'."
-    exit 1
-fi
-
-# Creating output files directory if not exists
-if [ ! -d "$output_dir" ]; then
-    mkdir "$output_dir"
-fi
 
 echo -e "$color_bright_cyan
 Output files are stored in $output_dir$color_reset"
-
-# Ask if the user wants to use Amass
-read -p "Do you want to use Amass for subdomain enumeration? (y/n): " amass_choice
-if [[ "$amass_choice" =~ ^[Yy]$ ]]; then
-    use_amass="yes"
-else
-    use_amass="no"
-fi
 
 # Tools Arsenal
 echo -e "$color_bright_cyan""Tools Arsenal:$color_reset"
 echo -e "- subfinder:   Subdomain discovery from various public sources."
 echo -e "- findomain:   The fastest and complete solution for domain recognition."
-echo -e "- shosubgo:    Small tool to Grab subdomains using Shodan API."
-echo -e "- amass:       Comprehensive subdomain enumeration (if selected)."
+echo -e "- shosubgo:    Small tool to Grab subdomains using Shodan API (if selected)."
+echo -e "- amass:       Comprehensive subdomain enumeration."
 echo -e "- httprobe:    Check live subdomains responding to HTTP requests."
-echo -e "- gowitness: 	a golang, web screenshot utility using Chrome Headless"
+echo -e "- gowitness:   A golang, web screenshot utility using Chrome Headless"
 echo -e "- gau:         (GetAllUrls) Extract URLs from web pages, including JS files."
 
 # Check for required tools
-for tool in subfinder findomain shosubgo httprobe gowitness gau ; do
+for tool in subfinder findomain shosubgo amass httprobe gowitness gau ; do
     if ! command -v "$tool" &> /dev/null; then
         echo -e "${color_bright_red}Error: $tool is not installed.${color_reset}"
         exit 1
@@ -144,82 +134,71 @@ run_command() {
 # Running subfinder
 echo -e "$color_bright_yellow
 Running subfinder:$color_reset"
-run_command subfinder -silent -d "$domain" -o "$output_dir/1_subfinder.txt" > /dev/null
-echo -e "Subfinder found $(wc -l "$output_dir/1_subfinder.txt")"
+run_command subfinder -silent -d "$domain" -o "$output_dir/subfinder.txt" > /dev/null
+echo -e "Subfinder found $(wc -l "$output_dir/subfinder.txt")"
 
 # Running Findomain
 echo -e "$color_bright_yellow
 Running findomain:$color_reset"
-run_command findomain -t "$domain" -u "$output_dir/2_findomain.txt" > /dev/null
-echo -e "Findomain found $(wc -l "$output_dir/2_findomain.txt")"
+run_command findomain -t "$domain" -u "$output_dir/findomain.txt" > /dev/null
+echo -e "Findomain found $(wc -l "$output_dir/findomain.txt")"
 
 # Running Shosubgo
 echo -e "$color_bright_yellow
 Running Shosubgo:$color_reset"
-run_command shosubgo -d "$domain" -s "$shodanAPI" > "$output_dir/3_shosubgo.txt"
-echo -e "Shosubgo found $(wc -l "$output_dir/3_shosubgo.txt")"
+if [ -n "$shodanAPI" ]; then
+    run_command shosubgo -d "$domain" -s "$shodanAPI" > "$output_dir/shosubgo.txt"
+    echo -e "Shosubgo found $(wc -l "$output_dir/shosubgo.txt")"
+fi
 
-# Running Amass (only if chosen by the user)
-if [ "$use_amass" = "yes" ]; then
-    echo -e "$color_bright_yellow
-    Running Amass => Passive mode:$color_reset"
-    run_command amass enum --passive -silent -d "$domain" -o "$output_dir/4.1_amassPassive.txt"
-    echo -e "Amass found $(wc -l "$output_dir/4.1_amassPassive.txt")"
+# Running Amass (Passive mode always, Active mode based on user input)
+echo -e "$color_bright_yellow
+Running Amass => Passive mode:$color_reset"
+run_command amass enum --passive -silent -d "$domain" -o "$output_dir/amassPassive.txt"
+echo -e "Amass found $(wc -l "$output_dir/amassPassive.txt")"
 
+# Ask if the user wants to use Active mode (subdomain brute-forcing)
+read -p "Do you want to use Amass Active mode (subdomain brute-forcing)? (y/n): " amass_active_choice
+if [[ "$amass_active_choice" =~ ^[Yy]$ ]]; then
     echo -e "$color_bright_yellow
     Running Amass => Active mode:$color_reset"
-    run_command amass enum -d "$domain" -brute -silent -w "$wordlist" -o "$output_dir/4.2_amassActive.txt"
-    echo -e "Amass found $(wc -l "$output_dir/4.2_amassActive.txt")"
+    run_command amass enum -d "$domain" -brute -silent -w "$wordlist" -o "$output_dir/amassActive.txt"
+    echo -e "Amass found $(wc -l "$output_dir/amassActive.txt")"
 fi
 
 # ALL IN ONE: Merge and sort subdomains, handling missing files
 {
-  [ -f "$output_dir/1_subfinder.txt" ] && cat "$output_dir/1_subfinder.txt"
-  [ -f "$output_dir/2_findomain.txt" ] && cat "$output_dir/2_findomain.txt"
-  [ -f "$output_dir/3_shosubgo.txt" ] && cat "$output_dir/3_shosubgo.txt"
-  [ "$use_amass" = "yes" ] && [ -f "$output_dir/4.1_amassPassive.txt" ] && cat "$output_dir/4.1_amassPassive.txt"
-  [ "$use_amass" = "yes" ] && [ -f "$output_dir/4.2_amassActive.txt" ] && cat "$output_dir/4.2_amassActive.txt"
-} | sort -u > "$output_dir/5_ALLSUBs.txt"
+  [ -f "$output_dir/subfinder.txt" ] && cat "$output_dir/subfinder.txt"
+  [ -f "$output_dir/findomain.txt" ] && cat "$output_dir/findomain.txt"
+  [ -f "$output_dir/shosubgo.txt" ] && cat "$output_dir/shosubgo.txt"
+  [ -f "$output_dir/amassPassive.txt "] && cat "$output_dir/amassPassive.txt"
+  [ "$use_amass" = "yes" ] && [ -f "$output_dir/amassActive.txt" ] && cat "$output_dir/amassActive.txt"
+} | sort -u > "$output_dir/AllSubs.txt"
 
 echo -e "$color_bright_red
-Merging & Sorting $(wc -l < "$output_dir/5_ALLSUBs.txt")$color_reset"
+Merging & Sorting $(wc -l < "$output_dir/AllSubs.txt")$color_reset"
 
 # Running httprobe
 echo -e "$color_bright_cyan
 Running httprobe:$color_reset"
-run_command cat "$output_dir/5_ALLSUBs.txt"  | httprobe -c 50 > "$output_dir/6_liveSubdomains.txt"
-echo -e "Httprobe found $(wc -l "$output_dir/6_liveSubdomains.txt")"
+run_command cat "$output_dir/AllSubs.txt" | httprobe -c 50 > "$output_dir/liveSubs.txt"
+echo -e "Httprobe found $(wc -l < "$output_dir/liveSubs.txt")"
 
 # Running Gowitness
 echo -e "$color_bright_green
 Running Gowitness to capture screenshots:$color_reset"
 if [ ! -d "$gowitness_output_dir" ]; then
-    mkdir "$gowitness_output_dir"
+    mkdir -p "$gowitness_output_dir"
 fi
-run_command gowitness scan file -f "$output_dir/6_liveSubdomains.txt" --threads 50 --ports-small --write-screenshots --screenshot-path "$gowitness_output_dir"
+run_command gowitness scan file -f "$output_dir/liveSubs.txt" --threads 50 --ports-small --write-screenshots --screenshot-path "$gowitness_output_dir"
 echo -e "Gowitness has captured screenshots and saved the report in $gowitness_output_dir"
 
 # Running gau
 echo -e "$color_bright_magenta
 Running gau(GetAllUrls)$color_reset"
 echo -e "Hint:  png,jpg,gif,jpeg are blacklisted"
-run_command cat "$output_dir/6_liveSubdomains.txt" | gau --threads 5 --blacklist png,jpg,gif,jpeg > "$output_dir/8_getallurls.txt"
-echo -e "Output in $output_dir/8_getallurls.txt"
+run_command cat "$output_dir/liveSubs.txt" | gau --threads 50 > "$output_dir/GetAllURLs.txt"
+echo -e "gau found $(wc -l < "$output_dir/GetAllURLs.txt")"
 
-##########################################################################################################################
-# FINISHED
-echo -e "$color_bright_cyan\nSummary of Results:$color_reset"
-echo -e "Subdomains found in subfinder: $(wc -l < "$output_dir/1_subfinder.txt")"
-echo -e "Subdomains found in findomain: $(wc -l < "$output_dir/2_findomain.txt")"
-echo -e "Subdomains found in shosubgo: $(wc -l < "$output_dir/3_shosubgo.txt")"
-
-if [ "$use_amass" = "yes" ]; then
-    echo -e "Subdomains found in Amass (Passive): $(wc -l < "$output_dir/4.1_amassPassive.txt")"
-    echo -e "Subdomains found in Amass (Active): $(wc -l < "$output_dir/4.2_amassActive.txt")"
-fi
-
-echo -e "Live subdomains found: $(wc -l < "$output_dir/6_liveSubdomains.txt")"
-echo -e "Total unique subdomains found: $(wc -l < "$output_dir/5_ALLSUBs.txt")"
-echo -e "gowitness output in: $gowitness_output_dir"
-echo -e "gau found: $(wc -l < "$output_dir/8_getallurls.txt") URLs"
-echo -e "$color_bright_green\nAll tasks completed successfully!${color_reset}"
+echo -e "$color_bright_green
+All tasks completed! Output is stored in $output_dir$color_reset"
